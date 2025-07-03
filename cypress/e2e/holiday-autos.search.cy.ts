f3b239cb788cb2d35392ce2518bbcc3eed7dd0d2
Xavier Casafont <Xavier.Casafont@amcsgroup.com>
2025-07-03 16:31:21 +0200

    adding select pick up and return dates

--- cypress/e2e/holiday-autos.search.cy.ts
+++ cypress/e2e/holiday-autos.search.cy.ts
@@ -3,8 +3,8 @@ import { HolidayAutosHomePage } from "@pages/HolidayAutosHomePage";
 describe("Holiday Autos - Search Test", () => {
   const homepage = new HolidayAutosHomePage();
 
-  it("should search cars using a valid date range", () => {
-
+  it("Test 1: Search for cars using pickup location and date range", () => {
     homepage.visit();
+    homepage.enterPickUpLocation('Barcelona');
   });
 });
--- cypress/pages/HolidayAutosHomePage.ts
+++ cypress/pages/HolidayAutosHomePage.ts
@@ -1,5 +1,24 @@
 export class HolidayAutosHomePage {
-  visit() {
-    cy.visit("https://www.holidayautos.com");
-  }
+    visit(): void {
+        cy.visit("https://www.holidayautos.com");
+        cy.url().should('contain', '/searchcars');
+    }
+
+    enterPickUpLocation(location: string): void {
+        let enterPickUpLocationSelector: string = '#pickupLocation';
+        cy.get(enterPickUpLocationSelector).click();
+        cy.get(enterPickUpLocationSelector).type(location).type('{enter}');
+        cy.get('#item-0-0').click()
+    }
+
+    selectPickUpAndReturnDates(pickupDate: Date, returnDate: Date): void {
+        cy.get('#pickupDate').click();
+        this.selectDate(pickupDate);
+
+        cy.get('#returnDate').click();
+        this.selectDate(returnDate);
+    }
+
+    selectDate(localDate: Date): void {
+    }
 }
